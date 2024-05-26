31c149dd7069a59497788080c2de46853539c1a9
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2024-05-26 19:23:02 +0200

    Generate resume from input data using gpt

---
+++ main.py
@@ -0,0 +1,59 @@
+from ResumeGen.resume import ResumeGPT
+import frontmatter
+import json
+from typing import Dict, Tuple
+
+
+
+def load_markdown(md_file) -> Tuple[dict, dict]:
+    with open(md_file, 'r') as file:
+        content = file.read()
+
+    # remove the front matter
+    content = frontmatter.loads(content)
+    return content.metadata, content.content
+
+
+
+
+
+# Main function
+def main():
+    personal_info, background_info = load_markdown('input/background_info.md')
+    _, job_description = load_markdown('input/job_description.md')
+
+    photo_path = 'input/photo.jpg'
+
+    generator = ResumeGPT(templates='templates')
+
+    resume_content = generator.collect_resume_data(job_description, background_info, personal_info)
+
+    include_photo = input("Do you want to include a photo in the resume? (y/n): ")
+    if include_photo == 'y':
+        resume_content['photo_path'] = photo_path
+
+    else:
+        resume_content['photo_path'] = None
+
+
+    # save the resume content to a file
+    with open('resume_content.json', 'w') as file:
+        json.dump(resume_content, file, indent=4)
+
+    # generate the resume
+    resume_latex = generator.create_latex_resume(resume_content)
+    with open('resume.tex', 'w') as file:
+        file.write(resume_latex)
+
+    
+    include_photo = input("Do you want to compile the resume to PDF? (y/n): ")
+    if include_photo == 'y':
+        # compile the latex file to PDF using pdflatex
+        import subprocess
+        subprocess.run(['pdflatex', 'resume.tex'])
+
+        print("Resume generated successfully!")
+    
+
+if __name__ == "__main__":
+    main()
